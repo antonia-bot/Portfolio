a406e2fe04d54d3996f619b1079f307c64933ca3
antonia <antoniamakosso05@gmail.com>
2025-11-13 15:03:05 +0100

    Débug

--- index.php
+++ index.php
@@ -6,6 +6,57 @@
     <title>PORT-FOLIO</title>
 </head>
 <body>
-     <h1></h1>
+    <!-- haut de la page -->
+    <header>
+        <h1>Bienvenue</h1>
+        <p>Bonjour, je suis Antonia, développeuse web junior passionnée par le code et le design. J’aime transformer des idées en projets concrets et fonctionnels, et je souhaite aujourd’hui mettre mes compétences au service de votre entreprise.</p>
+    </header>
+
+   <!-- Navigateur -->
+    <nav class="sidebar">
+        <h2>Menu</h2>
+        <ul>
+            <li><a href="acceuil"></a>Experience</li>
+            <li><a href="projets"></a>Projets</li>
+            <li><a href="contact"></a>Contact</li>
+        </ul>
+    </nav>
+
+    <!-- menu principal -->
+ <!-- <img src="" alt="">  -->
+    <main class="content">
+       <section>
+        <h3>Mes projets</h3>
+
+            <article>
+                <h1>Projets 1</h1> 
+                <p>Description</p>
+              <!--  <img src="./image/projet1.jpg" alt="" width="500"> -->
+            </article>
+
+            <article>
+                <h1>Projets 2/h1>
+                <p>Description</p>
+              <!--  <img src="./image/projet 2.jpg" alt="" width="500"> -->
+            </article>
+
+            <article>
+                <h1>Projets 3</h1>
+                <p>Description</p>
+              <!--  <img src="./image/projet 3.jpg" alt="" width="500"> -->
+            </article>
+
+       </section>   
+    </main>
+
+
+
+
+
+
+  <!-- pieds de la page -->
+    <footer>
+   <video src="" controls></video>
+    </footer>
 </body>
 </html>
